4306aa6755d5bf90a18c52478d5ec858eb6bb0f8
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-10-23 22:39:46 +0200

    Adding webGL image fingerprint from browserleaks.com

--- fp/detect.js
+++ fp/detect.js
@@ -72,7 +72,7 @@ function set_final_message() {
 }
 
 function incProgress() {
-	document.getElementById('progress').value += 10;
+	document.getElementById('progress').value += 100/9;
 }
 
 function ACTION() {
@@ -120,79 +120,30 @@ function ACTION() {
 	var canvasDataURI = getCanvasDataURI();
 	incProgress();
 	if (canvasDataURI) {
-		set_result('canvasDataURI', canvasDataURI);
-
 		if (canvasDataURI != "data:,") {
 			var img = document.getElementById('canvasImg');
 			img.src = canvasDataURI;
 			img.style.border = '2px solid navy';
 
 			set_fingerprint_data('canvasFpHash', canvasDataURI);
+		} else {
+			set_fingerprint_data('canvasFpHash', '');
 		}
+	} else {
+		set_fingerprint_data('canvasFpHash', '');
 	}
 	incProgress();
+
+	var webGLImageData = createWebGLImageAndReturnData();
+	if (webGLImageData == null) {
+		set_fingerprint_data('webGLFpHash', '');
+	} else {
+		set_fingerprint_data('webGLFpHash', JSON.stringify(webGLImageData));
+        }
+        incProgress();
 
 	set_message('Audio test results should appear below in about 1 second.');
 
 	// start reading sensor data
 	detectSensors();
-}
-
-function webGLBaseData() {
-    var result = [];
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    if (!ctx) {
-		return;
-    }
-
-    try {
-		set_result('glVersion', ctx.getParameter(ctx.VERSION));
-		set_result('glRenderer', ctx.getParameter(ctx.RENDERER));
-        set_result('glVendor', ctx.getParameter(ctx.VENDOR));
-    } catch (e) {
-    }
-}
-
-function webGLDebugData() {
-    var canvas = document.createElement('canvas');
-    var ctx1 = canvas.getContext("webgl"), ctx2 = canvas.getContext("experimental-webgl");
-
-    return setDebugData(ctx1) || setDebugData(ctx2);
-}
-
-function setDebugData(ctx) {
-    try {
-        if (ctx.getSupportedExtensions().indexOf("WEBGL_debug_renderer_info") >= 0) {
-			var ext = ctx.getExtension('WEBGL_debug_renderer_info');
-			set_result('glDebugVendor', ctx.getParameter(ext.UNMASKED_VENDOR_WEBGL));
-			set_result('glDebugRenderer', ctx.getParameter(ext.UNMASKED_RENDERER_WEBGL));
-
-			return true;
-        }
-    } catch (e) {
-    }
-    return false;
-}
-
-function getCanvasDataURI() {
-	try {
-		var canvas = document.createElement('canvas');
-		canvas.height = 60;
-		canvas.width = 400;
-		canvasContext = canvas.getContext("2d");
-		canvas.style.display = "inline";
-		canvasContext.textBaseline = "alphabetic";
-		canvasContext.fillStyle = "#f60";
-		canvasContext.fillRect(125, 1, 62, 20);
-		canvasContext.fillStyle = "#069";
-		canvasContext.font = "11pt no-real-font-123";
-		canvasContext.fillText("Cwm fjordbank glyphs vext quiz, \ud83d\ude03", 2, 15);
-		canvasContext.fillStyle = "rgba(102, 204, 0, 0.7)";
-		canvasContext.font = "18pt Arial";
-		canvasContext.fillText("Cwm fjordbank glyphs vext quiz, \ud83d\ude03", 4, 45);
-		return canvas.toDataURL();
-	} catch(e){
-		return false;
-	}
 }
